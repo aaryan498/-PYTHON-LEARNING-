dd2e9966fd42cb15b656f27eeb4e2b8533c44448
aaryan498 <kumaraaryan498.com>
2025-07-19 19:03:29 +0530

    DAY 1 Final Submisson

--- Git-Tutorial/TUTORIAL.py
+++ Git-Tutorial/TUTORIAL.py
@@ -31,6 +31,8 @@ git push -u origin main → Uploads your local files to GitHub for the first tim
 '''
 
 # Process to do 
+
+# STEP 1: Connecting Folder with Repository
 '''
 git init
 Response ~ Initialized empty Git repository in C:/Users/homeuser/Desktop/#BeginningPythonPractice/.git/
@@ -43,14 +45,21 @@ git remote -v
 Response:
 origin  https://github.com/aaryan498/-PYTHON-LEARNING-.git (fetch)
 origin  https://github.com/aaryan498/-PYTHON-LEARNING-.git (push)
+'''
 
+# STEP 2: Staging Your Files ~ Ready to Push
+'''
 git add .
 Response ~ No Response
 This adds everything (folders and files) to the staging area.
 
 CHECK STATUS
 git status
+
 Response ~ 
+
+Example1 ~ This happens when you change anything in the file after typing command 'git add .' in the terminal: (FAIL)
+
 On branch master
 
 No commits yet
@@ -66,6 +75,51 @@ Changes not staged for commit:
   (use "git restore <file>..." to discard changes in working directory)
         modified:   #GitTutorial/TUTORIAL.py
 
+Example 2: when you dont change anything after the 'git add .' command:  (SUCCESS)
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   #Day1-Variables&Datatypes/code1.py
+        new file:   #Day2/code2.py
+        new file:   #GitTutorial/TUTORIAL.py
+
+
         
 
+
+        
+git commit -m "Initial commit: Added #Day1, #Day2, #GitTutorial folders"
+Here in Double Quotes is the message That is general so describe in own words about the changes
+
+Response ~ 
+[master (root-commit) d2e0b62] Initial commit: Added #Day1, #Day2, #GitTutorial
+ 3 files changed, 136 insertions(+)
+ create mode 100644 #Day1-Variables&Datatypes/code1.py
+ create mode 100644 #Day2/code2.py
+ create mode 100644 #GitTutorial/TUTORIAL.py
 '''
+
+# STEP 4: Pushing the Folders and Files
+'''
+Since your branch is currently named master but GitHub uses main, we’ll fix that and then push:
+git branch -M main
+
+
+git push -u origin main
+NOTE: This code is only for first time push
+'''
+# Files get pushed into the Repository SUCCESSFULLY....!
+
+
+# For further changes in the Files ~ USE COMMAND
+'''
+git add .
+git commit -m "Your message"
+git push
+
+AND THE CHANGES GETS PUSHED INTO THE REPOSITORY
+'''
+STATUS="COMPLETED SUCCESSFULLY"
+print("The Git Pushing Tutorial is",STATUS,sep=" ~ ")
